ee948093dadd319c76e086fc7eab8e187ccc8e77
vitito <vitito61@gmail.com>
2017-09-27 18:10:31 +0000

    Uploaded index.js after due date

--- items.js
+++ items.js
@@ -27,31 +27,7 @@ function ItemDAO(database) {
     this.getCategories = function(callback) {
         "use strict";
 
-        /*
-        * TODO-lab1A
-        *
-        * LAB #1A: Implement the getCategories() method.
-        *
-        * Write an aggregation query on the "item" collection to return the
-        * total number of items in each category. The documents in the array
-        * output by your aggregation should contain fields for "_id" and "num".
-        *
-        * HINT: Test your mongodb query in the shell first before implementing
-        * it in JavaScript.
-        *
-        * In addition to the categories created by your aggregation query,
-        * include a document for category "All" in the array of categories
-        * passed to the callback. The "All" category should contain the total
-        * number of items across all categories as its value for "num". The
-        * most efficient way to calculate this value is to iterate through
-        * the array of categories produced by your aggregation query, summing
-        * counts of items in each category.
-        *
-        * Ensure categories are organized in alphabetical order before passing
-        * to the callback.
-        *
-        */
-        // var categories = [];
+        
         
         var itemsPipeline = function(db, callback) {
             var collection = db.collection('item');
@@ -84,12 +60,7 @@ function ItemDAO(database) {
           );
         };
         itemsPipeline(this.db,callback);
-        // TODO-lab1A Replace all code above (in this method).
-
-        // TODO Include the following line in the appropriate
-        // place within your code to pass the categories array to the
-        // callback.
-        // callback(categories);
+       
     };
 
 
